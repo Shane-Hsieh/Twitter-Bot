b71337f7dbf877c31ce153c25fb3d62416480b47
Shane Hsieh <71193209+Shane-Hsieh@users.noreply.github.com>
2021-01-05 00:03:26 -0500

    Create Twitter-Bot.py

---
+++ Twitter-Bot.py
@@ -0,0 +1,32 @@
+import tweepy
+import time
+
+// You just enter your tokens from the API here, blank because I'm not sharing mine.
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+def limit_handle(cursor):
+  while True:
+    try:
+      yield cursor.next()
+    except tweepy.RateLimitError:
+      time.sleep(1000)
+
+for follower in limit_handle(tweepy.Cursor(api.followers).items()):
+  if follower.name == 'Usernamehere':
+    print(follower.name)
+    follower.follow()
+
+for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+    try:
+        tweet.favorite()
+        print('Retweeted the tweet')
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
